c83a705ae12bed666534f4982198d4194fd6c29b
Sara Gjikdhima <saragjikdhima@gmail.com>
2020-04-18 18:19:04 +0200

    crated login and signup php files

---
+++ php/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+ <body>
+     <form action="" method="post">
+         <h1>Login</h1>
+         <p>Email</p>
+         <input type="email" name="email">
+         <p>Password</p>
+         <input type="password" name="password">
+     </form>
+     <p>Don't have an account yet?</p>
+     <a href="signup.php">Sign Up</a>
+ </body>
+</html>
