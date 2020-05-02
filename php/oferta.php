e968f5684f5aae7a1d28eb2de47dda8894dfce27
Sara Gjikdhima <saragjikdhima@gmail.com>
2020-05-02 03:18:20 +0200

    modifications

--- php/oferta.php
+++ php/oferta.php
@@ -1,22 +1,65 @@
 <!-- Faqja e ofertave -->
 
-<?php 
+<?php
 
     require_once('db_connect.php');
 
-        $stmt = $conn->prepare('SELECT id, sale, start_date, end_date, description, image FROM Offer');
-        $stmt->bind_param();
+    $query = "SELECT * FROM Offer";  
+    $result = mysqli_query($conn, $query); 
 
-        
-        $stmt->execute();
-        $stmt->store_result();
-        $stmt->bind_result($id, $sale, $start_date, $end_date, $description, $image);
-        $stmt->fetch();
-        
-        while ($s = $stmt->fetch()) {
-            # code...
+    $stmt = "SELECT admin FROM User";
+    $res = mysqli_query($conn, $stmt);
+
+    while ($row = mysqli_fetch_array($result) && $s = mysqli_fetch_array($res)) {
+        if ($s['admin'] == 1 && $row[0]) {
+            echo '<div class="carousel-item active">';
+            echo '<img class="d-block w-100" src="../img/favicon.png" alt="First slide">';
+            echo '<div class="carousel-caption d-none d-md-block">';
+            echo '<h3>' .  $row['sale'] . '</h3>';
+            echo '<p>' . $row['description'] . '</p>';
+            echo '<p>' . $row['start_date'] . '</p>';
+            echo '<p>' . $row['end_date'] . '</p>';
+            echo '<button type="button" class="btn btn-success">Porosit</button>';
+            echo '<button type="button" class="btn btn-primary">Modifiko</button>
+                <button type="button" class="btn btn-danger">Fshi</button>';
+            echo '</div>';
+            echo '</div>';
+        } else {
+            echo '<div class="carousel-item">';
+            echo '<img class="d-block w-100" src="../img/favicon.png" alt="First slide">';
+            echo '<div class="carousel-caption d-none d-md-block">';
+            echo '<h3>' .  $row['sale'] . '</h3>';
+            echo '<p>' . $row['description'] . '</p>';
+            echo '<p>' . $row['start_date'] . '</p>';
+            echo '<p>' . $row['end_date'] . '</p>';
+            echo '<button type="button" class="btn btn-success">Porosit</button>';
+            echo '<button type="button" class="btn btn-primary">Modifiko</button>
+                <button type="button" class="btn btn-danger">Fshi</button>';
+            echo '</div>';
+            echo '</div>';
         }
-
-    
-
+        if ($row[0]) {
+            echo '<div class="carousel-item active">';
+            echo '<img class="d-block w-100" src="../img/favicon.png" alt="First slide">';
+            echo '<div class="carousel-caption d-none d-md-block">';
+            echo '<h3>' .  $row['sale'] . '</h3>';
+            echo '<p>' . $row['description'] . '</p>';
+            echo '<p>' . $row['start_date'] . '</p>';
+            echo '<p>' . $row['end_date'] . '</p>';
+            echo '<button type="button" class="btn btn-success">Porosit</button>';
+            echo '</div>';
+            echo '</div>';
+        } else {
+            echo '<div class="carousel-item">';
+            echo '<img class="d-block w-100" src="../img/favicon.png" >';
+            echo '<div class="carousel-caption d-none d-md-block">';
+            echo '<h3>' .  $row['sale'] . '</h3>';
+            echo '<p>' . $row['description'] . '</p>';
+            echo '<p>' . $row['start_date'] . '</p>';
+            echo '<p>' . $row['end_date'] . '</p>';
+            echo '<button type="button" class="btn btn-success">Porosit</button>';
+            echo '</div>';
+            echo '</div>';
+        }
+    }
 ?>
