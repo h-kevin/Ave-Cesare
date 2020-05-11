a532bfa892af4912526bbf12657e7031552935de
Kevin <kevin.haxhi@outlook.com>
2020-05-11 03:13:13 +0200

    worked on html and css for profile page, added ajax and php to fetch profile data from the db, implemented logout function

--- js/profile.js
+++ js/profile.js
@@ -1,1 +1,57 @@
 // Javascript/jQuery file for profile page
+
+// on document ready request for user info
+$(document).ready(function () {
+
+    $.ajax({
+        type: "post",
+        url: "../php/profile.php",
+        dataType: "json",
+        success: function (response) {
+            onResponse(response);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+        }
+    });
+});
+
+// function to execute on ajax response
+function onResponse (pinfo) {
+    $('.ribbon-content').html(`<p><b>Pershendetje ${ pinfo['name'] }!</b></p>`);
+    $('.liname').text(`Emri: ${ pinfo['name'] }`);
+    $('.lisur').text(`Mbiemri: ${ pinfo['surname'] }`);
+    $('.limail').text(`E-mail: ${ pinfo['email'] }`);
+    $('.prof-img').css('background-image', `url('${ pinfo['prof_img'] }'`);
+
+    if (!pinfo['mobile'])
+        $('.litel').text(`Telefon: jo`);
+    else
+        $('.litel').text(`Telefon: ${ pinfo['mobile'] }`);
+};
+
+// profile image on click open modal2
+$('.prof-img').click(function () {
+    $('#modal2').modal('toggle');
+});
+
+// on logout button clicked logout
+$('.logout').click(function () {
+    $.ajax({
+        type: "get",
+        url: "../php/logout.php",
+        success: function (response) {
+            if (response == 'loggedOut') {
+                window.location.replace('login.html');
+                $('.logout .spinner-border').removeClass('d-none');
+            }
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(`Ndodhi nje problem: ${ xrh.status }`);
+            alert(thrownError);
+        }
+    }).done(function () {
+        $('.logout .spinner-border').addClass('d-none');
+    });
+});
---
+++ php/profile.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * PHP file to handle profile page requests
+ */
+
+    // check if session is active
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    // create json object with session data
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $infoObj->id = $_SESSION['id'];
+        $infoObj->email = $_SESSION['email'];
+        $infoObj->name = $_SESSION['name'];
+        $infoObj->surname = $_SESSION['surname'];
+        $infoObj->password = $_SESSION['password'];
+        $infoObj->verified = $_SESSION['verified'];
+        $infoObj->prof_img = $_SESSION['prof_img'];
+        $infoObj->admin = $_SESSION['admin'];
+
+        $infoObj = json_encode($infoObj);
+
+        echo $infoObj;
+    }
+
+?>
