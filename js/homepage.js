ae587836f155bce19211de4dae7ea9851a889a13
neraida <neraidamuho@gmail.com>
2020-05-17 22:22:29 +0200

    index.html homepage.css homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -3,15 +3,15 @@
 function navBarBackgroundScrollEffect () {
   $(window).scroll(function () {
     if ($(window).width() < 767) {
-      if ($(window).scrollTop() >= 450) {
-        $('.navigationbar').css('background-color', 'rgba(30, 30, 30, 0.7)');
+      if ($(window).scrollTop() >= 90) {
+        $('.navigationbar').css('background-color', 'rgba(30, 30, 30, 0.8)');
       } else {
         $('.navigationbar').css('background-color', 'transparent');
       }
     }
     else {
-      if ($(window).scrollTop() >= 600) {
-        $('.navigationbar').css('background-color', 'rgba(30, 30, 30, 0.7)');
+      if ($(window).scrollTop() >= 500) {
+        $('.navigationbar').css('background-color', 'rgba(30, 30, 30, 0.8)');
       } else {
         $('.navigationbar').css('background-color', 'transparent');
       }
