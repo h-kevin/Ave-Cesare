e9d9225be85f93e85e0a7f58782661f9beb4f7e1
saragjikdhima <saragjikdhima@gmail.com>
2020-05-23 16:36:56 +0200

    fixed bugs

--- php/offers.php
+++ php/offers.php
@@ -10,28 +10,15 @@
     echo '<div class="row justify-content-center">';
 
     while ($row = mysqli_fetch_array($result)) {
-        echo '<div class="card col-lg-3 col-md-4 col-sm-12 ml-3 mb-3">';
-        echo '<img src="' . $row['image'] . '" class="card-img-top h-50 img-fluid">';
-        echo '<div class="card-body text-center d-flex flex-column">';
-        echo '<h6 class="card-title">SUPER OFERTE!</h6>';
-        echo '<p class="card-text">' . date('d-m-Y', strtotime($row['start_date'])) . ' - ' . date('d-m-Y', strtotime($row['end_date'])) . 
-            '<br>'. $row['description']. '<br>';
+        echo '<div class="card col-lg-3 col-md-4 col-sm-12 ml-3 mb-3 px-0 mr-3 karta">';
+        echo '<img class="card-img-top h-50 oferta" src="' . $row['image'] . '" alt="Foto oferta">';
+        echo '<div class="card-body permbajtja">';
+        echo '<h4 class="card-title">' . $row['name'] . '</h4>';
+        echo '<p class="card-text mb-1">' . $row['description'] . '<br>';
 
-            $stmt_ing = $conn->prepare("SELECT name FROM (Product INNER JOIN Prod_Offer ON Prod_Offer.prod_id=Product.id)
-                         WHERE Prod_Offer.offer_id=?");
-            $stmt_ing->bind_param('i', $row['id']);
-            $stmt_ing->execute();
-            $res_ing = $stmt_ing->get_result();
-
-            $ing_list = '';
-            while ($ing = mysqli_fetch_array($res_ing)) {
-                $ing_list .= $ing['name'];
-                $ing_list .= ', ';
-            }
-            echo substr($ing_list ,0,-2);
-        echo '<p>Totali: ' . $row['sale'] . ' Lek</p>';
+        echo 'Kurse: ' . $row['discount'] . ' Lek';
         echo '</p>';
-        echo '<button type="button" class="btn btn-success mt-auto">Perfito</button>';
+        echo '<a class="btn buton-karte">Përfito!</a>';
         echo '</div>';
         echo '</div>';
     }
