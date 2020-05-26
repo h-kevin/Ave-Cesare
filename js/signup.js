956b0340f6217cd63df6a863a2fd71dd3cc8497c
neraida <neraidamuho@gmail.com>
2020-05-26 19:44:40 +0200

    fixed some bugs

--- js/signup.js
+++ js/signup.js
@@ -9,13 +9,13 @@ $(document).ready(function(){
 
         if (name != "" && !regex1.test(name)) {
             $('#errBox').html('Vendosni emrin ne formatin e kerkuar! Vetem karaktere alfabetike, pa hapesira ose simbole');
-            $('#errBox').addClass('alert alert-primary');
+            $('#errBox').addClass('alert alert-light');
             $('#regjistrohu').addClass('disabled');
             $('#regjistrohu').attr('disabled',true);
         }
         else{
             $('#errBox').html('');
-            $('#errBox').removeClass('alert alert-primary');
+            $('#errBox').removeClass('alert alert-light');
             $('#regjistrohu').removeClass('disabled');
             $('#regjistrohu').attr('disabled',false);
         }
@@ -26,13 +26,13 @@ $(document).ready(function(){
 
         if (surname != "" && !regex1.test(surname)) {
             $('#errBox').html('Vendosni mbiemrin ne formatin e kerkuar! Vetem karaktere alfabetike, pa hapesira ose simbole');
-            $('#errBox').addClass('alert alert-primary');
+            $('#errBox').addClass('alert alert-light');
             $('#regjistrohu').addClass('disabled');
             $('#regjistrohu').attr('disabled',true);
         }
         else{
             $('#errBox').html('');
-            $('#errBox').removeClass('alert alert-primary');
+            $('#errBox').removeClass('alert alert-light');
             $('#regjistrohu').removeClass('disabled');
             $('#regjistrohu').attr('disabled',false);
         }
@@ -42,14 +42,14 @@ $(document).ready(function(){
         let pass1 = $('#password1').val();
 
         if(pass1.length < 4){
-            $('#errBox').addClass('alert alert-primary');
+            $('#errBox').addClass('alert alert-light');
             $('#errBox').html('Fjalekalimi duhet te kete te pakten 4 karaktere');
             $('#regjistrohu').addClass('disabled');
             $('#regjistrohu').attr('disabled',true);
         }
         else{
             $('#errBox').html('');
-            $('#errBox').removeClass('alert alert-primary');
+            $('#errBox').removeClass('alert alert-light');
             $('#regjistrohu').removeClass('disabled');
             $('#regjistrohu').attr('disabled',false);
         }
@@ -59,14 +59,14 @@ $(document).ready(function(){
         let pass2 = $('#password2').val();
         
         if(pass1 != pass2){
-            $('#errBox').addClass('alert alert-primary');
+            $('#errBox').addClass('alert alert-light');
             $('#errBox').html('Rishkruani te njejtin fjalekalim');
             $('#regjistrohu').addClass('disabled');
             $('#regjistrohu').attr('disabled',true);
         }
         else{
             $('#errBox').html('');
-            $('#errBox').removeClass('alert alert-primary');
+            $('#errBox').removeClass('alert alert-light');
             $('#regjistrohu').removeClass('disabled');
             $('#regjistrohu').attr('disabled',false);
         }
@@ -81,27 +81,27 @@ $(document).ready(function(){
         let pass2 = $('#password2').val();
         
         // if(pass1 != pass2){
-        //     $('#errBox').addClass('alert alert-primary');
+        //     $('#errBox').addClass('alert alert-light');
         //     $('#errBox').html('Fjalekalimet nuk perputhen. Provoni serish! <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
         //     setTimeout(function(){ 
         //         $('#errBox').html('');
-        //         $('#errBox').removeClass('alert alert-primary');}, 3000);
+        //         $('#errBox').removeClass('alert alert-light');}, 3000);
         // // }
         // if (name != "" && !regex1.test(name)) {
         //     $('#errBox').html('Vendosni emrin ne formatin e kerkuar! Vetem karaktere alfabetike, pa hapesira ose simbole');
-        //     $('#errBox').addClass('alert alert-primary');
+        //     $('#errBox').addClass('alert alert-light');
         //     setTimeout(function(){ 
         //         $('#errBox').html('');
-        //         $('#errBox').removeClass('alert alert-primary');}, 3000);
+        //         $('#errBox').removeClass('alert alert-light');}, 3000);
         // }
         // if (surname != "" && !regex1.test(surname)) {
         //     $('#errBox').html('Vendosni mbiemrin ne formatin e kerkuar! Vetem karaktere alfabetike, pa hapesira ose simbole');
-        //     $('#errBox').addClass('alert alert-primary');
+        //     $('#errBox').addClass('alert alert-light');
         //     setTimeout(function(){ 
         //         $('#errBox').html('');
-        //         $('#errBox').removeClass('alert alert-primary');}, 3000);
+        //         $('#errBox').removeClass('alert alert-light');}, 3000);
         // }
-            $('#errBox').addClass('alert alert-primary');
+            $('#errBox').addClass('alert alert-light');
             $('#errBox').html('Duke shtuar perdoruesin...  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
             $.ajax({
                 url:"../php/signup.php",
@@ -116,7 +116,7 @@ $(document).ready(function(){
                     $('#signup .spinner-border').addClass('d-none');
                     setTimeout(function(){ 
                         $('#errBox').html('');
-                        $('#errBox').removeClass('alert alert-primary');}, 5000);
+                        $('#errBox').removeClass('alert alert-light');}, 5000);
                     setTimeout(function(){ 
                         window.location.replace('login.html?register=ok');}, 5000);
                 },
